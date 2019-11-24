8affea1675ae4125467e0124172f06abd4b7b4cb
JBMembrado <jean-baptiste.membrado@polytechnique.edu>
2019-11-24 11:26:22 +0100

    OO Toolbox for reverb dataset

---
+++ reverb_data.py
@@ -0,0 +1,93 @@
+import re
+import os
+from speech_dataset_load import load_speech_dataset
+import reverb_model
+import shutil
+from datetime import datetime
+
+class ReverbData(object):
+    def __init__(self):
+        self.raw_dataset = None
+        self.raw_only_speech = None
+        self.raw_only_noise = None
+        self.new_path = None
+        self.new_folder_name = None
+        self.reverb_model = None
+
+    def load_dataset(self, subset=10):
+        """
+        Calls the load_speech_dataset function that creates a Dataset object from the GSD.
+        """
+        print('Loading the Google Speech Dataset.')
+        print()
+        self.raw_dataset = load_speech_dataset(subset)
+        print('Google Speech Dataset successfully loaded.')
+        print()
+
+
+    def separate_dataset(self):
+        """
+        Separates the dataset into speech/noise.
+        """
+        print('Separate dataset between speech and noise :')
+        self.raw_only_speech = self.raw_dataset.filter(speech=True)
+        self.raw_only_noise = self.raw_dataset.filter(speech=False)
+        print('Successfully separated the dataset.')
+
+    def init_new_folder(self):
+        """
+        This method creates and initiates the subfolders containing each wav file.
+        """
+        self.new_folder_name = 'data_reverb'
+        self.new_path = re.sub('data', self.new_folder_name, self.raw_dataset.basedir)
+
+        print('The new folder is located at : {0}'.format(self.new_path))
+        if not os.path.isdir(self.new_path):
+            os.makedirs(self.new_path)
+
+        for subdir in self.raw_dataset.subdirs:
+            extracted_subdir = re.split('/', subdir)[-2]
+            if not os.path.exists(self.new_path + '/' + extracted_subdir):
+                os.makedirs(self.new_path + '/' + extracted_subdir)
+
+        print('New Folder initiated.')
+
+    def init_reverb_model(self):
+        self.reverb_model = reverb_model.ReverbModel()
+        # example_audio_sample = self.raw_only_speech[0].meta.file_loc
+        self.reverb_model.init_reverb_model()
+
+    def apply_reverb(self):
+        print('Applying reverberation algorithm to the whole dataset : ')
+
+        for idx_sample, sample in enumerate(self.raw_only_speech):
+            self.init_reverb_model()
+            audio_sample_no_rev = sample.data
+            output_path = re.sub('data', self.new_folder_name, sample.meta.file_loc)
+            self.reverb_model.transform_audio(audio_sample_no_rev, output_path)
+
+            if (idx_sample%500 == 0) and (idx_sample > 0):
+                print('Now treating sample {0}.'.format(idx_sample))
+
+        print('Reverberation applied to the whole speech dataset.')
+
+    def copy_files(self):
+        testing_list = self.raw_dataset.basedir + '/testing_list.txt'
+        validation_list = self.raw_dataset.basedir + '/validation_list.txt'
+
+        shutil.copy2(testing_list, re.sub('data', self.new_folder_name, testing_list))
+        shutil.copy2(validation_list, re.sub('data', self.new_folder_name, validation_list))
+        print('Extra files copied.')
+
+
+start = datetime.now()
+
+rev_data = ReverbData()
+rev_data.load_dataset(subset=None)
+rev_data.separate_dataset()
+rev_data.init_new_folder()
+rev_data.apply_reverb()
+rev_data.copy_files()
+
+time_elapsed = datetime.now() - start
+print('Time elapsed (hh:mm:ss.ms) {0}'.format(time_elapsed))
---
+++ reverb_model.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+import time
+import numpy as np
+import pyroomacoustics as pra
+from scipy.io import wavfile
+
+
+class ReverbModel(object):
+    def __init__(self):
+        self.room_dimensions = None
+        self.shoebox = None
+        self.absorption = None
+        self.fs = None
+        self.max_order = None
+        self.mic = None
+        self.source_position = None
+
+    def init_reverb_model(self, absorption = 0.2, max_order = 15):
+        self.room_dimensions = [6, 5, 7]
+        self.absorption = absorption
+        self.max_order = max_order
+        self.fs = 16000
+
+        self.shoebox = pra.ShoeBox(
+            self.room_dimensions,
+            absorption=self.absorption,
+            fs=self.fs,
+            max_order=self.max_order,
+        )
+
+        self.source_position = [2, 3.1, 2]
+        self.mic = pra.MicrophoneArray(np.array([[2, 1.5, 2]]).T, self.shoebox.fs)
+
+    def transform_audio(self, audio_sample, output_path):
+        """
+        Applies reverberation to the audio_sample.
+        The source in the virtual room plays the audio_sample signal.
+        The output is the result of the captation of the microphone in the virtual room.
+        :param audio_sample: data signal we want to apply reverb on
+        :param output_path: path where we want to store the output of the reverberation algorithm
+        """
+        self.shoebox.add_source(self.source_position, signal=audio_sample)
+        self.shoebox.add_microphone_array(self.mic)
+
+        self.shoebox.simulate()
+        self.shoebox.mic_array.to_wav(output_path, norm=True, bitdepth=np.int16)
